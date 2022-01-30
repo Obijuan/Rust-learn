e3110a16be3d861e7639d283f3db68a2a3d2b486
obijuan <obijuan.cube@gmail.com>
2022-01-30 12:47:51 +0100

    Example added

---
+++ gcd/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    println!("Hello, world!");
+    let n = gcd(20,10);
+
+    println!("Resultado: {}", n);
+}
+
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0); 
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
